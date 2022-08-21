291e44c5e5ae4fcc374fe204be691ce411e29fd1
delberss <delberss@hotmail.com>
2022-08-20 22:10:31 -0300

    Projeto criado e Comment component  criado

---
+++ src/components/Comment.jsx
@@ -0,0 +1,13 @@
+import './Comment.css'
+
+const Comment = () => {
+    return(
+        <div className="comment">
+            <h2>Delber Soares</h2>
+            <p>O Delber está aprendendo ReactJS</p>
+            <p>2022-20-08</p>
+        </div>
+    )
+}
+
+export default Comment;
