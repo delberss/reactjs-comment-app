e629d44379619ebc7dae8f559b5342f77f2b66c9
delberss <delberss@hotmail.com>
2022-08-22 17:22:44 -0300

    Criado o CommentForm component e seu style

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,4 +1,4 @@
-import './Comment.css'
+import './style.css'
 import ProfileImage from './profile.png';
 
 const Comment = (props) => {
---
+++ src/components/CommentForm/index.jsx
@@ -0,0 +1,46 @@
+import './style.css'
+
+const CommentForm = (props) => {
+    const {onAddComment, onFieldChanged, form} = props;
+
+    return (
+        <form className="comment-form" onSubmit={onAddComment}>
+          <h2>Add a comment</h2>
+          <div>
+            <input 
+              onChange={onFieldChanged}
+              value={form.name}
+              type="text"
+              name="name"
+              placeholder="Type here your name"
+              required="required"
+              />
+          </div>
+
+          <div>
+            <input
+              onChange={onFieldChanged}
+              value={form.email}
+              type="email"
+              name="email"
+              placeholder="Type here your email"
+              required="required"
+            />
+          </div>
+
+          <div>
+            <textarea 
+              onChange={onFieldChanged}
+              value={form.message}
+              name="message"
+              rows="4"
+              placeholder="Type here your message"
+              required="required"
+              />
+          </div>
+          <button type="submit">Add a comment</button>
+        </form>
+    );
+}
+
+export default CommentForm;
